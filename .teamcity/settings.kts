c624f704d1da78cb4044086c7a761a3c32f9a2f8
meghasinghal <meghasinghal@TeamCity>
2022-10-13 17:04:32 +0530

    TeamCity change in 'JenKinsPipelineDemo' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,62 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.DotnetMsBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetMsBuild
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2022.04"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dotnetBuild {
+            name = "build"
+            enabled = false
+            configuration = "Release"
+            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        }
+        dotnetMsBuild {
+            projects = "WebGoat.NET.sln"
+            version = DotnetMsBuildStep.MSBuildVersion.V17
+            args = "-restore -noLogo"
+            sdk = "3.5"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
