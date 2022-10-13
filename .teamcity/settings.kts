0a515a9190fce11eedfa260f3d340ac12689a7cc
meghasinghal <meghasinghal@TeamCity>
2022-10-13 22:13:01 +0530

    TeamCity change in 'JenKinsPipelineDemo' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -82,6 +82,80 @@ object Build : BuildType({
             args = "-restore -noLogo"
             sdk = "3.5"
         }
+        powerShell {
+            name = "uploadZip"
+            scriptMode = script {
+                content = """
+                    Write-Host "Received scanning request successfully.."
+                    					
+                    ${'$'}sourcePath = "%teamcity.build.checkoutDir%"
+                    ${'$'}filePath = Split-Path -Path "${'$'}sourcePath"
+                    ${'$'}filePath += "\%env.TEAMCITY_PROJECT_NAME%-%build.number%.zip"
+                    Write-Host "${'$'}destinationPath"
+                    
+                    ${'$'}buildId = "%env.TEAMCITY_PROJECT_NAME%-%build.number%.zip"
+                    ${'$'}projectId = ${'$'}null
+                    
+                    if("${'$'}env:Offensive360SastApi_ProjectId" -ne "")
+                    {
+                    	${'$'}projectId = "${'$'}env:Offensive360SastApi_ProjectId"
+                    }
+                    
+                    ${'$'}projectName = "Zenkins_Project_${'$'}buildId"
+                    ${'$'}boundary = [System.Guid]::NewGuid().ToString()
+                    
+                    Write-Host "Starting scanning for the project name [${'$'}projectName], accessToken [${'$'}env:Offensive360SastApi_AccessToken], url [${'$'}env:Offensive360SastApi_BaseUrl], buildId [${'$'}buildId], filePath [${'$'}filePath], boundary [${'$'}boundary], projectId [${'$'}env:Offensive360SastApi_ProjectId]"
+                    
+                    ${'$'}fileBytes = [System.IO.File]::ReadAllBytes(${'$'}filePath)
+                    ${'$'}fileContent = [System.Text.Encoding]::GetEncoding('iso-8859-1').GetString(${'$'}fileBytes)
+                    
+                    ${'$'}LF = "`r`n"
+                    ${'$'}bodyLines = (
+                        "--${'$'}boundary",
+                        "Content-Disposition: form-data; name=`"projectOrRepoName`"${'$'}LF",
+                        "${'$'}projectName",
+                        "--${'$'}boundary",
+                        "Content-Disposition: form-data; name=`"projectID`"${'$'}LF",
+                        "${'$'}projectId",
+                        "--${'$'}boundary",
+                        "Content-Disposition: form-data; name=`"deleteProjectAndScanAfterScanning`"${'$'}LF",
+                        "false",
+                        "--${'$'}boundary",
+                        "Content-Disposition: form-data; name=`"projectSource`"; filename=`"${'$'}projectName.zip`"",
+                        "Content-Type: application/x-zip-compressed${'$'}LF",
+                        ${'$'}fileContent,
+                        "--${'$'}boundary--${'$'}LF"
+                    ) -join ${'$'}LF
+                    
+                    ${'$'}apiResponse = Invoke-RestMethod -Method Post -Uri ("{0}/app/api/ExternalScan/single-file" -f ${'$'}env:Offensive360SastApi_BaseUrl.TrimEnd('/')) -ContentType "multipart/form-data; boundary=`"${'$'}boundary`"" -Headers @{"Accept" = "application/json"; "Authorization" = "Bearer ${'$'}env:Offensive360SastApi_AccessToken"} -Body ${'$'}bodyLines
+                    
+                    write-host ("total vulnerabilities count = {0}" -f ${'$'}apiResponse.vulnerabilities.length)
+                    
+                    if (${'$'}apiResponse.vulnerabilities.length -gt 0 -and "${'$'}env:ADO_BreakBuildWhenVulnsFound" -eq \'True\') 
+                    {
+                        write-host "\n\n**********************************************************************************************************************"
+                        write-host ("Offensive 360 vulnerability dashboard : {0}/Scan/showscan-{1}-{2}" -f ${'$'}env:Offensive360SastUi_BaseUrl.TrimEnd('/'), ${'$'}apiResponse.projectId, ${'$'}apiResponse.id)
+                        write-host "**********************************************************************************************************************\n\n"
+                        throw [System.Exception] "Vulnerabilities found and breaking the build."
+                    }
+                    elseif (${'$'}apiResponse.vulnerabilities.length -gt 0 -and "${'$'}env:ADO_BreakBuildWhenVulnsFound" -ne 'True') 
+                    {
+                    	Write-Warning 'Vulnerabilities found and since ADO_BreakBuildWhenVulnsFound is set to false so continuing to build it.'
+                    }
+                    else
+                    {
+                    	Write-Warning 'No vulnerabilities found and continuing to build it.'
+                    }
+                    
+                    Write-Host "Finished SAST file scanning."
+                    
+                    if(Test-Path -Path ${'$'}destinationPath -PathType Leaf)
+                    {
+                    Remove-Item ${'$'}destinationPath
+                    }
+                """.trimIndent()
+            }
+        }
     }
 
     triggers {
