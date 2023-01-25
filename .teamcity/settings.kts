bb069a043da20dc48b70da939f1677914249979e
Megha Singhal <meghaaeron1987@gmail.com>
2023-01-25 19:22:47 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,27 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetMsBuild
 import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
 
 version = "2022.04"
 
@@ -67,6 +88,7 @@ object Build : BuildType({
                     
                     ${'$'}buildId = "%env.TEAMCITY_PROJECT_NAME%-%build.number%.zip"
                     ${'$'}projectId = ${'$'}null
+                    ${'$'}sourceType = 4 #TeamCity
                     
                     if("%env.Offensive360SastApi_ProjectId%" -ne "")
                     {
@@ -76,7 +98,7 @@ object Build : BuildType({
                     ${'$'}projectName = "TeamCity_Project_${'$'}buildId"
                     ${'$'}boundary = [System.Guid]::NewGuid().ToString()
                     
-                    Write-Host "Starting scanning for the project name [${'$'}projectName], accessToken [%env.Offensive360SastApi_AccessToken%], url [%env.Offensive360SastApi_BaseUrl%], buildId [${'$'}buildId], filePath [${'$'}filePath], boundary [${'$'}boundary], projectId [%env.Offensive360SastApi_ProjectId%], IsBreakBuildWhenVulnsFound [%env.ADO_BreakBuildWhenVulnsFound%], Offensive360SastApi_SCA [%env.Offensive360SastApi_SCA%], Offensive360SastApi_Malware [%env.Offensive360SastApi_Malware%], Offensive360SastApi_Licence [%env.Offensive360SastApi_Licence%]"
+                    Write-Host "Starting scanning for the project name [${'$'}projectName], accessToken [%env.Offensive360SastApi_AccessToken%], url [%env.Offensive360SastApi_BaseUrl%], buildId [${'$'}buildId], filePath [${'$'}filePath], boundary [${'$'}boundary], projectId [%env.Offensive360SastApi_ProjectId%], IsBreakBuildWhenVulnsFound [%env.ADO_BreakBuildWhenVulnsFound%], Offensive360SastApi_AllowDependencyScan [%env.Offensive360SastApi_AllowDependencyScan%], Offensive360SastApi_AllowMalwareScan [%env.Offensive360SastApi_AllowMalwareScan%], Offensive360SastApi_AllowLicenseScan [%env.Offensive360SastApi_AllowLicenseScan%]"
                     
                     ${'$'}fileBytes = [System.IO.File]::ReadAllBytes(${'$'}filePath)
                     ${'$'}fileContent = [System.Text.Encoding]::GetEncoding('iso-8859-1').GetString(${'$'}fileBytes)
@@ -84,38 +106,45 @@ object Build : BuildType({
                     ${'$'}LF = "`r`n"
                     ${'$'}bodyLines = (
                         "--${'$'}boundary",
-                        "Content-Disposition: form-data; name=`"projectOrRepoName`"${'$'}LF",
+                        "Content-Disposition: form-data; name=`"name`"${'$'}LF",
                         "${'$'}projectName",
                         "--${'$'}boundary",
-                        "Content-Disposition: form-data; name=`"projectID`"${'$'}LF",
+                        "Content-Disposition: form-data; name=`"projectId`"${'$'}LF",
                         "${'$'}projectId",
                         "--${'$'}boundary",
-                        "Content-Disposition: form-data; name=`"deleteProjectAndScanAfterScanning`"${'$'}LF",
+                        "Content-Disposition: form-data; name=`"keepInvisibleAndDeletePostScan`"${'$'}LF",
                         "false",
                         "--${'$'}boundary",
-                        "Content-Disposition: form-data; name=`"sCA`"${'$'}LF",
-                        "%env.Offensive360SastApi_SCA%",
+                        "Content-Disposition: form-data; name=`"allowDependencyScan`"${'$'}LF",
+                        "%env.Offensive360SastApi_AllowDependencyScan%",
                          "--${'$'}boundary",
-                        "Content-Disposition: form-data; name=`"malware`"${'$'}LF",
-                        "%env.Offensive360SastApi_Malware%",
+                        "Content-Disposition: form-data; name=`"allowMalwareScan`"${'$'}LF",
+                        "%env.Offensive360SastApi_AllowMalwareScan%",
                          "--${'$'}boundary",
-                        "Content-Disposition: form-data; name=`"license`"${'$'}LF",
-                        "%env.Offensive360SastApi_License%",
+                        "Content-Disposition: form-data; name=`"allowLicenseScan`"${'$'}LF",
+                        "%env.Offensive360SastApi_AllowLicenseScan%",
                         "--${'$'}boundary",
-                        "Content-Disposition: form-data; name=`"projectSource`"; filename=`"${'$'}projectName.zip`"",
+                        "Content-Disposition: form-data; name=`"externalScanSourceType`"${'$'}LF",
+                        "${'$'}sourceType",
+                        "--${'$'}boundary",
+                        "Content-Disposition: form-data; name=`"fileSource`"; filename=`"${'$'}projectName.zip`"",
                         "Content-Type: application/x-zip-compressed${'$'}LF",
                         ${'$'}fileContent,
                         "--${'$'}boundary--${'$'}LF"
                     ) -join ${'$'}LF
                     
-                    ${'$'}apiResponse = Invoke-RestMethod -Method Post -Uri ("{0}/app/api/ExternalScan/single-file" -f "%env.Offensive360SastApi_BaseUrl%".TrimEnd('/')) -ContentType "multipart/form-data; boundary=`"${'$'}boundary`"" -Headers @{"Accept" = "application/json"; "Authorization" = "Bearer %env.Offensive360SastApi_AccessToken%"} -Body ${'$'}bodyLines
+                    ${'$'}apiResponse = Invoke-RestMethod -Method Post -Uri ("{0}/app/api/externalScan" -f "%env.Offensive360SastApi_BaseUrl%".TrimEnd('/')) -ContentType "multipart/form-data; boundary=`"${'$'}boundary`"" -Headers @{"Accept" = "application/json"; "Authorization" = "Bearer %env.Offensive360SastApi_AccessToken%"} -Body ${'$'}bodyLines
                     
                     write-host ("total vulnerabilities count = {0}" -f ${'$'}apiResponse.vulnerabilities.length)
+                    write-host ("Total Vulnerabilities Count : {0}" -f ${'$'}apiResponse.vulnerabilities.length)
+                    write-host ("Total Malwares Count : {0}" -f ${'$'}apiResponse.malwares.length)
+                    write-host ("Total Licenses Count : {0}" -f ${'$'}apiResponse.licenses.length)
+                    write-host ("Total Dependency Vulnerabilities Count : {0}" -f ${'$'}apiResponse.dependencyVulnerabilities.length)
                     
-                    if (${'$'}apiResponse.vulnerabilities.length -gt 0 -and "%env.ADO_BreakBuildWhenVulnsFound%" -eq 'True') 
+                    if ((${'$'}apiResponse.vulnerabilities.length -gt 0 -or ${'$'}apiResponse.malwares.length -gt 0 -or ${'$'}apiResponse.licenses.length -gt 0 -or ${'$'}apiResponse.dependencyVulnerabilities.length -gt 0) -and "%env.ADO_BreakBuildWhenVulnsFound%" -eq 'True') 
                     {
                         write-host "**********************************************************************************************************************"
-                        write-host ("Offensive 360 vulnerability dashboard : {0}/Scan/showscan-{1}-{2}" -f "%env.Offensive360SastUi_BaseUrl%".TrimEnd('/'), ${'$'}apiResponse.projectId, ${'$'}apiResponse.id)
+                        write-host ("Offensive 360 vulnerability dashboard : {0}/scan/{1}" -f "%env.Offensive360SastUi_BaseUrl%".TrimEnd('/'), ${'$'}apiResponse.projectId)
                         write-host "**********************************************************************************************************************"
                         throw [System.Exception] "Vulnerabilities found and breaking the build."
                     }
